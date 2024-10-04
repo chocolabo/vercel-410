33a0065be6cf7402666661f56aed48b3d8dd2010
chocolabo <134146517+chocolabo@users.noreply.github.com>
2024-10-04 17:14:31 +0900

    Update 410.ts

--- api/410.ts
+++ api/410.ts
@@ -4,18 +4,26 @@ export default async (
     request: NowRequest,
     response: NowResponse
 ): Promise<void> => {
-    response.setHeader("cache-control", "s-maxage=31536000");
-    response.setHeader("Content-Type", "text/html");  // HTMLを返すためのContent-Typeを指定
+    const { url } = request;
 
-    // HTMLとCSSを組み込んだレスポンス
-    const htmlContent = `
-        <!DOCTYPE html>
-        <html lang="ja">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>410 Gone</title>
-            <style>
+    if (url === "/robots.txt") {
+        // robots.txtの内容を返す
+        response.setHeader("Content-Type", "text/plain");
+        response.send(`User-agent: *
+Disallow: /`);
+    } else {
+        // 410 Gone のHTMLページを返す
+        response.setHeader("cache-control", "s-maxage=31536000");
+        response.setHeader("Content-Type", "text/html");
+
+        const htmlContent = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>410 Gone</title>
+           <style>
                 body {
                     background-color: #efefef;
                     color: #161209;
@@ -36,23 +44,20 @@ export default async (
                     font-size: 1em;
                 }
             </style>
-        </head>
-        <body>
-            <div class="container">
-                <h1>みるくちょこ.ink 跡地（410 Gone）</h1>
-                <p>誠に勝手ながら『みるくちょこ.ink』は閉鎖いたしました。<br />
-身体的およびメンタルの不調により、今後サーバーの維持が困難になる可能性が高く、予期せぬトラブルを避けるため、このような決断をいたしました。<br /><br />
+            </head>
+            <body>
+                <div class="container">
+                    <h1>みるくちょこ.ink 跡地（410 Gone）</h1>
+                    <p>誠に勝手ながら『みるくちょこ.ink』は閉鎖いたしました。<br />
+                    身体的およびメンタルの不調により、今後サーバーの維持が困難になる可能性が高く、予期せぬトラブルを避けるため、このような決断をいたしました。<br /><br />
+                    調子が戻りましたら、いつかどこかで復活するかもしれません。<br />
+                    その際はよろしくお願いいたします。<br /><br />
+                    長らくのお付き合い、誠にありがとうございました。</p>
+                </div>
+            </body>
+            </html>
+        `;
 
-調子が戻りましたら、いつかどこかで復活するかもしれません。<br />
-その際はよろしくお願いいたします。<br /><br />
-
-長らくのお付き合い、誠にありがとうございました。</p>
-            </div>
-        </body>
-        </html>
-    `;
-
-    // HTMLをレスポンスとして送信
-    response.status(410).send(htmlContent);
-    return;
+        response.status(410).send(htmlContent);
+    }
 };
